641c3dfba58f6ffde6425d316d16d217c794b86d
Michael Sorhaindo <michael@exmachina.nl>
2019-12-05 14:43:03 +0100

    day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,44 +1,67 @@
 f = open('input.txt')
 line = f.readline()
 
-program_pointer = 0
 ADD_OP_CODE = 1
 MULT_OP_CODE = 2
 
-program_list = [int(s) for s in line.split(',')]
+def load_program():
+  return [int(s) for s in line.split(',')]
 
-def ammend_program():
-  program_list[1] = 12
-  program_list[2] = 2
 
-def run_add_operation(program_list, program_pointer):
-  a = program_list[program_list[program_pointer+1]]
-  b = program_list[program_list[program_pointer+2]]
-  program_list[program_list[program_pointer+3]] = a + b
+def ammend_program(noun, verb, program_list):
+  program_list[1] = noun
+  program_list[2] = verb
   return program_list
 
-def run_mult_operation(program_list, program_pointer):
-  a = program_list[program_list[program_pointer+1]]
-  b = program_list[program_list[program_pointer+2]]
-  program_list[program_list[program_pointer+3]] = a * b
+def run_add_operation(program_list, instruction_pointer):
+  a = program_list[program_list[instruction_pointer+1]]
+  b = program_list[program_list[instruction_pointer+2]]
+  program_list[program_list[instruction_pointer+3]] = a + b
   return program_list
 
-def  run_operation_at_pointer(program_list, program_pointer):
-  if(program_list[program_pointer] is ADD_OP_CODE):
-    return run_add_operation(program_list, program_pointer)
-  if(program_list[program_pointer] is MULT_OP_CODE):
-    return run_mult_operation(program_list, program_pointer)
+def run_mult_operation(program_list, instruction_pointer):
+  a = program_list[program_list[instruction_pointer+1]]
+  b = program_list[program_list[instruction_pointer+2]]
+  program_list[program_list[instruction_pointer+3]] = a * b
+  return program_list
 
-def  process_program(program_list, program_pointer):
-  program_list = run_operation_at_pointer(program_list, program_pointer)
-  program_pointer = program_pointer + 4
-  print(program_list[program_pointer])
-  if (program_list[program_pointer] is 99):
-    return True
+def  run_operation_at_pointer(program_list, instruction_pointer):
+  if(program_list[instruction_pointer] is ADD_OP_CODE):
+    return run_add_operation(program_list, instruction_pointer)
+  if(program_list[instruction_pointer] is MULT_OP_CODE):
+    return run_mult_operation(program_list, instruction_pointer)
+
+def process_program(program_list, instruction_pointer):
+  program_list = run_operation_at_pointer(program_list, instruction_pointer)
+  instruction_pointer = instruction_pointer + 4
+  if (program_list[instruction_pointer] is 99):
+    return program_list[0]
   else:
-    process_program(program_list, program_pointer)
+    return process_program(program_list, instruction_pointer)
 
-ammend_program()
-process_program(program_list, program_pointer)
+def find_noun_verb():
+  noun = 0
+  verb = 0
+  output = 0
+  original_program_list = load_program()
+  while output != 19690720:
+    program_list = load_program()
+    ammend_program(noun, verb, program_list)
+    try:
+      output = process_program(program_list, 0) # 0 puts instruction pointer at start
+    except:
+      output = 0
+    if output == 19690720:
+      return (noun,verb)
+    if verb < 100:
+      verb = verb + 1
+    elif verb is 100:
+      verb = 0
+      noun = noun + 1 
+    elif noun is 100:
+      return (-1, -1)
+  return (noun, verb)
 
-print(program_list)
+noun, verb = (find_noun_verb())
+
+print(100 * noun + verb)
