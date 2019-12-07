1f93fe80ac37e73305f7907255aec4a53ea84b0a
Michael Sorhaindo <michael@exmachina.nl>
2019-12-07 16:05:12 +0100

    day 5 part 1 cracked - turns out it was good all along minus the outputs! XD

--- day5/day5.py
+++ day5/day5.py
@@ -5,6 +5,10 @@ ADD_OP_CODE = 1
 MULT_OP_CODE = 2
 INPUT_OP_CODE = 3
 OUTPUT_OP_CODE = 4
+JMP_IF_TRUE_OP_CODE = 5
+JMP_IF_FALSE_OP_CODE = 6
+LESS_THAN_OP_CODE = 7
+EQUALS_OPCODE = 8
 
 def load_program():
   return [int(s) for s in line.split(',')]
@@ -19,14 +23,12 @@ def run_add_operation(program_list, instruction_pointer, parsed_instruction):
   a = program_list[program_list[instruction_pointer+1]] if int(parsed_instruction[2]) == 0 else program_list[instruction_pointer+1]
   b = program_list[program_list[instruction_pointer+2]] if int(parsed_instruction[1]) == 0 else program_list[instruction_pointer+2]
   program_list[program_list[instruction_pointer+3]] = a + b
-  print(a,b, a+b)
   return (program_list, 4)
 
 def run_mult_operation(program_list, instruction_pointer, parsed_instruction):
   a = program_list[program_list[instruction_pointer+1]] if int(parsed_instruction[2]) == 0 else program_list[instruction_pointer+1]
   b = program_list[program_list[instruction_pointer+2]] if int(parsed_instruction[1]) == 0 else program_list[instruction_pointer+2]
   program_list[program_list[instruction_pointer+3]] = a * b
-  print(a,b, a*b)
   return (program_list, 4)
 
 def run_input_operation(program_list, instruction_pointer, parsed_instruction):
@@ -34,14 +36,13 @@ def run_input_operation(program_list, instruction_pointer, parsed_instruction):
   return (program_list, 2)
 
 def run_output_operation(program_list, instruction_pointer, parsed_instruction):
-  print(program_list[instruction_pointer+1])
+  print(program_list[program_list[instruction_pointer+1]])
   return (program_list, 2)
 
 def run_operation_at_pointer(program_list, instruction_pointer):
   parsed_instruction = list(str(program_list[instruction_pointer]))
   while len(parsed_instruction) != 5:
     parsed_instruction.insert(0,0)
-  print(parsed_instruction, program_list[instruction_pointer+1], program_list[instruction_pointer+2])
   if(program_list[instruction_pointer] % 100 is ADD_OP_CODE):
     return run_add_operation(program_list, instruction_pointer, parsed_instruction)
   if(program_list[instruction_pointer] % 100 is MULT_OP_CODE):
@@ -49,6 +50,14 @@ def run_operation_at_pointer(program_list, instruction_pointer):
   if(program_list[instruction_pointer] % 100 is INPUT_OP_CODE):
     return run_input_operation(program_list, instruction_pointer, parsed_instruction)
   if(program_list[instruction_pointer] % 100 is OUTPUT_OP_CODE):
+    return run_output_operation(program_list, instruction_pointer, parsed_instruction)
+  if(program_list[instruction_pointer] % 100 is JMP_IF_TRUE_OP_CODE):
+    return run_output_operation(program_list, instruction_pointer, parsed_instruction)
+  if(program_list[instruction_pointer] % 100 is JMP_IF_FALSE_OP_CODE):
+    return run_output_operation(program_list, instruction_pointer, parsed_instruction)
+  if(program_list[instruction_pointer] % 100 is LESS_THAN_OP_CODE):
+    return run_output_operation(program_list, instruction_pointer, parsed_instruction)
+  if(program_list[instruction_pointer] % 100 is EQUALS_OPCODE):
     return run_output_operation(program_list, instruction_pointer, parsed_instruction)
 
 def process_program(program_list, instruction_pointer):
