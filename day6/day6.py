0e808939f95cd91952ea5bdb760f1925601f6e7d
Michael Sorhaindo <michael@exmachina.nl>
2019-12-09 10:33:43 +0100

    completes day 6 orbit depth thanks treelib!

--- day6/day6.py
+++ day6/day6.py
@@ -44,12 +44,25 @@ def calculate_orbits():
   for x in tree.all_nodes():
     indirect_orbits = indirect_orbits + (tree.depth(x.identifier) -1)
 
-  print(direct_orbits + indirect_orbits)
+  print('direct + indirect',  direct_orbits + indirect_orbits)
 
 def minimum_orbit_transfers():
   santa = tree.get_node('SAN')
   me = tree.get_node('YOU')
-  print('depths', tree.depth(santa), tree.depth(me))
+  transfers = 0
+
+  while me.identifier != santa.identifier:
+    if tree.depth(me) > tree.depth(santa):
+      me = tree.parent(me.identifier)
+      transfers = transfers + 1
+    elif tree.depth(santa) > tree.depth(me):
+      santa = tree.parent(santa.identifier)
+      transfers = transfers + 1
+    else:
+      me = tree.parent(me.identifier)
+      santa = tree.parent(santa.identifier)
+      transfers = transfers + 2
+  print('transfers', transfers - 2)
 
 calculate_orbits()
 minimum_orbit_transfers()
